e79bd197c6a62c753fb62abc56d1e735d4547997
Bruno Dhein <dhein.brunoruan@gmail.com>
2020-08-08 12:25:09 -0300

    finished day three

--- src/components/teacherItem/index.tsx
+++ src/components/teacherItem/index.tsx
@@ -1,36 +1,53 @@
 import React from 'react'
+import api from '../../services/api'
 
-import './styles.css'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
-function TeacherItem(){
+import './styles.css'
+
+export interface Teacher {
+    avatar: string,
+    bio: string,
+    cost: number,
+    id: number,
+    name: string,
+    subject: string,
+    whatsapp: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) =>{
+    function createNewConnection() {
+        api.post("connections", {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
         <header>
-            <img src="https://avatars2.githubusercontent.com/u/56564508?s=460&u=a75c5e4fbf16a5b1b60aa10d2a7287f2a428ecaf&v=4" alt="foto do professor"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-                <strong>Bruno Dhein</strong>
-                <span>Computação</span>
+                <strong>{teacher.name}</strong>
+                <span>{teacher.subject}</span>
             </div>
         </header>
 
-        <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not 
-        <br /><br />
-        only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-        including versions of Lorem Ipsum
-        </p>
+        <p>{teacher.bio}</p>
 
     <footer>
         <p>
             Preço/hora
-            <strong>R$50,00</strong>
+            <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a onClick={createNewConnection} href={` https://wa.me/${teacher.whatsapp}`} target="_blank">
             <img src={whatsappIcon} alt="Whatsapp"/>
             Entrar em contato
-        </button>
+        </a>
     </footer>
     </article>
     )
